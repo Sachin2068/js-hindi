0d1bc1a270115165fce7695bf3f8c45e31d346d7
Sachin Kumar Maharana <141555139+Sachin2068@users.noreply.github.com>
2025-04-03 18:59:19 +0000

    data types done

---
+++ basics/test.js
@@ -0,0 +1,9 @@
+let accountEmail = "g@.com"
+var accountPassword = "1234"
+city = "jaipur"
+
+accountEmail = "schn@google.com"
+accountPassword = "123"
+city = "blr"
+
+console.table([accountEmail, accountPassword, city])
